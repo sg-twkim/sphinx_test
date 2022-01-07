63ee7f88d6adb90f48cfaadd3c501d6933d87ce6
sg-twkim <twkim1@seegene.com>
2022-01-07 16:21:59 +0900

    Initial commit

---
+++ doc_test/__init__.py
@@ -0,0 +1,11 @@
+"""
+    doc_test
+    ~~~~~~~~
+    
+    Google style docstring documentation 테스트입니다.
+    
+    :copyright: (c) 2022 by Taewook Kim.
+    :license: Public Domain.
+"""
+
+__version__ = '1.0.0'
